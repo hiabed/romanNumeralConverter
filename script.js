38f532e68b639b4f546ba7458110069e105be9a7
Mohammed-Abed Hassani-Idrissi <mhassani@c3r9p4.1337.ma>
2024-07-02 14:27:24 +0100

    small update

--- script.js
+++ script.js
@@ -10,33 +10,35 @@ function convert()
         output.innerHTML = "Please enter a number greater than or equal to 1.";
     else if (parseInt(inputText.value, 10) > 3999)
         output.innerHTML = "Please enter a number less than or equal to 3999";
-
-    let rCharacters = ["I", "V", "X", "L", "C", "D", "M"];
-    let input = inputText.value.split('').reverse().join('');
-    let result = "";
-    for (let i = 0; i < input.length; i++)
+    else
     {
-        if (parseInt(input[i], 10) <= 3)
+        let rCharacters = ["I", "V", "X", "L", "C", "D", "M"];
+        let input = inputText.value.split('').reverse().join('');
+        let result = "";
+        for (let i = 0; i < input.length; i++)
         {
-            for (let j = 0; j < parseInt(input[i], 10); j++)
-                result += rCharacters[0];
+            if (parseInt(input[i], 10) <= 3)
+            {
+                for (let j = 0; j < parseInt(input[i], 10); j++)
+                    result += rCharacters[0];
+            }
+            else if (parseInt(input[i], 10) === 4)
+                result += rCharacters[1] + rCharacters[0];
+            else if (parseInt(input[i], 10) === 5)
+                result += rCharacters[1];
+            else if (parseInt(input[i], 10) === 6)
+                result +=  rCharacters[0] + rCharacters[1];
+                else if (parseInt(input[i], 10) === 7)
+                result += rCharacters[0] + rCharacters[1] + rCharacters[1];
+            else if (parseInt(input[i], 10) === 8)
+                result += rCharacters[0] + rCharacters[1] + rCharacters[1] + rCharacters[1];
+            else if (parseInt(input[i], 10) === 9)
+                result += rCharacters[2] + rCharacters[0];
+            rCharacters.splice(0, 2);
         }
-        else if (parseInt(input[i], 10) === 4)
-            result += rCharacters[1] + rCharacters[0];
-        else if (parseInt(input[i], 10) === 5)
-            result += rCharacters[1];
-        else if (parseInt(input[i], 10) === 6)
-            result +=  rCharacters[0] + rCharacters[1];
-            else if (parseInt(input[i], 10) === 7)
-            result += rCharacters[0] + rCharacters[1] + rCharacters[1];
-        else if (parseInt(input[i], 10) === 8)
-            result += rCharacters[0] + rCharacters[1] + rCharacters[1] + rCharacters[1];
-        else if (parseInt(input[i], 10) === 9)
-            result += rCharacters[2] + rCharacters[0];
-        rCharacters.splice(0, 2);
+        result = result.split('').reverse().join('');
+        output.innerHTML = result;
     }
-    result = result.split('').reverse().join('');
-    output.innerHTML = result;
     inputText.value = "";
 }
 
